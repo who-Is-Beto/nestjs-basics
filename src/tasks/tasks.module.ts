bccc72658a7a4cd305e06c1aed5131e857729921
whoisbeto <roberto.cortes@htech.com>
2021-10-05 13:45:14 -0500

    mongo problem

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
-import { TaskSchema, task } from './scheemas/task.scheema';
+import { TaskSchema, Task } from './scheemas/task.scheema';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: task.name, schema: TaskSchema }]),
+    MongooseModule.forFeatureAsync([
+      {
+        name: Task.name,
+        useFactory: () => {
+          const schema = TaskSchema;
+          schema.pre('save', function () {
+            console.log('Hello from pre save');
+          });
+          return schema;
+        },
+      },
+    ]),
   ],
   controllers: [TasksController],
   providers: [TasksService],
